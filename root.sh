d8e6ca8435264106fe43f2da2280b8fd473e8c9d
ari <ari@instance-4.us-central1-a.c.ftpshare.internal>
2020-08-17 15:05:46 +0000

    new root

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
-sudo apt-get install texlive-full
-sudo apt-get install texmaker
-sudo apt-get install ffmpeg
-sudo apt-get install espeak
+sudo apt-get install texlive-full -y
+sudo apt-get install texmaker -y
+sudo apt-get install ffmpeg -y 
+sudo apt-get install espeak -y 
 sudo cp policy.xml /etc/ImageMagick-6/policy.xml 
 
 
