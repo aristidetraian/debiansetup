02b15870ca171d8c5a899482d551bc7476ad8403
root <root@phonix.workgroup>
2020-08-17 08:07:19 -0400

    update after a first test

--- root.sh
+++ root.sh
@@ -1,10 +1,7 @@
 sudo apt-get install texlive-full
 sudo apt-get install texmaker
 sudo apt-get install ffmpeg
-ffmpeg -version
 sudo apt-get install espeak
-sudo apt-get install latex
-sudo apt-get install pdf2img
-sudo cp /etc/ImageMagick-6/policy.xml 
-sudo apt-get install texlive-latex-extra
+sudo cp policy.xml /etc/ImageMagick-6/policy.xml 
+
 
